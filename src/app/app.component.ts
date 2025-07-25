f28a9bd4df03604c80085e8244cec5ff4d8fc404
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2025-07-25 20:35:51 -0300

    gera relatorio e paginacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface Student {
+  id: number;
+  name: string;
+  age: number;
+  average: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ProjectRodarteNogueiraClient';
-  selectedFile: File | null = null;
-
   constructor(private http: HttpClient) {}
+
+  selectedFile: File | null = null;
+  students: Student[] = [];
+  pagedStudents: Student[] = [];
+
+  currentPage = 1;
+  pageSize = 5;
+  totalPages = 0;
 
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -28,8 +40,62 @@ export class AppComponent {
     formData.append('file', this.selectedFile);
 
     this.http.post('http://localhost:8080/student', formData).subscribe({
-      next: response => console.log('Upload concluído:', response),
+      next: response => alert("Dados processados com sucesso!"),
       error: err => console.error('Erro no upload:', err)
+    });
+  }
+
+  onListStudents() {
+    this.http.get<Student[]>('http://localhost:8080/student').subscribe({
+      next: response => {
+        this.students = response;
+        this.totalPages = Math.ceil(this.students.length / this.pageSize);
+        this.updatePagedStudents();
+      },
+      error: err => console.error('Erro ao consultar alunos:', err)
+    });
+  }
+
+  updatePagedStudents() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedStudents = this.students.slice(startIndex, endIndex);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePagedStudents();
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagedStudents();
+    }
+  }
+
+  onReport() {
+    this.http.get('http://localhost:8080/student/export', {
+      responseType: 'blob'
+    }).subscribe({
+      next: (response: Blob) => {
+        const blob = new Blob([response], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const url = window.URL.createObjectURL(blob);
+
+        // Abre ou força o download
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'relatorio_rodarte.xlsx';
+        a.click();
+
+        // Limpeza
+        window.URL.revokeObjectURL(url);
+      },
+      error: (err) => {
+        console.error('Erro ao baixar o relatório:', err);
+      }
     });
   }
 }
